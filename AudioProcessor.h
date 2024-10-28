1fe9a38f9ad9c662b6ae5c84d35af2e06ea07a84
Speechrezz <thespeechlessmusic@gmail.com>
2024-10-28 16:25:10 -0700

    Initial Commit

---
+++ AudioProcessor.h
@@ -0,0 +1,63 @@
+#include <emscripten/webaudio.h>
+#include <iostream>
+#include <stdlib.h>
+
+namespace xynth
+{
+
+class AudioBufferWASM
+{
+public:
+	AudioBufferWASM(const AudioSampleFrame* audioFrame)
+	{
+		numChannels = audioFrame[0].numberOfChannels;
+		numSamples = audioFrame[0].samplesPerChannel;
+		data = audioFrame[0].data;
+	}
+
+	int getNumChannels() const { return numChannels; }
+	int getNumSamples() const { return numSamples; }
+
+	float* getChannelPointer(int channelIndex)
+	{
+		return data + channelIndex * numSamples;
+	}
+
+protected:
+	float* data = nullptr;
+	int numChannels = 0, numSamples = 0;
+
+};
+
+struct ProcessSpec
+{
+	int sampleRate;
+	int numChannels;
+	int maxBlockSize;
+};
+
+class AudioProcessor
+{
+public:
+	AudioProcessor() = default;
+
+	void prepare(const ProcessSpec& spec)
+	{
+
+	}
+
+	void process(AudioBufferWASM& outputBuffer)
+	{
+		for (int channel = 0; channel < outputBuffer.getNumChannels(); ++channel)
+		{
+			auto* output = outputBuffer.getChannelPointer(channel);
+			for (int i = 0; i < outputBuffer.getNumSamples(); ++i)
+			{
+				output[i] = (rand() / (float)RAND_MAX * 2.0f - 1.0f) * 0.1f;
+			}
+		}
+	}
+
+};
+
+}
---
+++ build.sh
@@ -0,0 +1,1 @@
+emcc -g AudioWorklet.cpp -o AudioWorklet.js -s WASM_WORKERS=1 -s AUDIO_WORKLET=1 -s WEBAUDIO_DEBUG=1
