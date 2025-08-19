1245440fe4fe6a5068319c18bd97326c8d810a65
Speechrezz <thespeechlessmusic@gmail.com>
2025-08-19 13:36:45 -0700

    Folder naming consistency

---
+++ src/synth/SynthProcessor.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "xylo_audio/AudioCore.h"
+#include "xylo_audio/AudioBuffer.h"
+#include "xylo_audio/AudioParameter.h"
+#include "xylo_midi/MidiView.h"
+#include "NoteVector.h"
+#include "SynthVoice.h"
+#include <array>
+
+namespace xynth
+{
+
+class SynthProcessor
+{
+public:
+    static constexpr size_t maxVoiceCount = 16;
+
+public:
+    SynthProcessor(xylo::AudioParameters&);
+
+    void prepare(const xylo::ProcessSpec&);
+    void process(xylo::AudioView& audioView, xylo::MidiView& midiView);
+
+protected:
+    void renderSynthVoices(xylo::AudioView&, int prevSamplePos, int samplesBetweenEvents);
+    void handleMidiEvent(const xylo::MidiEvent&);
+
+    void noteOn (const xylo::MidiEvent&);
+    void noteOff(const xylo::MidiEvent&);
+
+    void reclaimVoices();
+
+protected:
+    xylo::AudioParameters& parameters;
+    std::array<SynthVoice, maxVoiceCount> synthVoiceList;
+    
+    NoteVector<maxVoiceCount> activeNoteList;
+    xylo::SmallVector<size_t, maxVoiceCount> inactiveVoiceList;
+
+    xylo::dsp::ADSRProcessor::Parameters adsrParameters;
+
+};
+
+}
---
+++ src/synth/SynthVoice.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "SineOscillator.h"
+#include "xylo_audio/AudioBuffer.h"
+#include "xylo_dsp/ADSRProcessor.h"
+
+namespace xynth
+{
+
+class SynthVoice
+{
+public:
+    void prepare(const xylo::ProcessSpec&);
+    void renderNextBlock(xylo::AudioView& audioView);
+
+    void startNote(int midiNoteNumber, float velocity, int currentPitchWheelPosition);
+    void stopNote(float velocity, bool allowTailOff = true);
+
+    bool isCurrentPlaying() const 
+    { 
+        return adsr.isCurrentlyPlaying(); 
+    }
+
+    void updateADSR(const xylo::dsp::ADSRProcessor::Parameters&);
+
+protected:
+    xylo::AudioBuffer voiceBuffer;
+    SineOscillator osc;
+    xylo::dsp::ADSRProcessor adsr;
+    float frequency = 0.f, velocity = 1.f;
+    bool noteOnFlag = false;
+
+};
+
+}
